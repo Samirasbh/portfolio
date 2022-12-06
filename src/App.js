fe347015bfd1a42b53ea907ba8c4b29090688166
Samira <116953785+Samirasbh@users.noreply.github.com>
2022-12-07 01:12:07 +0330

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import About from "./components/About/About";
 import Header from "./components/Header/Header";
 
 function App() {
   return(
     <>
     <Header/>
+    <About/>
     </>
   )
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,16 @@
+import "./About.css"
+
+const About =()=>{
+    return(
+        <>
+        <div className="about">
+            <div className="about-cover">
+                <img src="" alt="" />
+            </div>
+            <div className="about-content"></div>
+        </div>
+        </>
+    )
+}
+
+export default About
